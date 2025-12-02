f97804b24033461bb0728279ec839d8544fdde80
Arjun Biju <arjunbiju200611@gmail.com>
2025-12-02 16:58:59 +0530

    third commit

---
+++ No_due_frontend/src/admin/Admin_login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Admin_login() {
+  return (
+    <div>
+        <h1>Admin login</h1>
+    </div>
+  )
+}
+
+export default Admin_login
+
--- No_due_frontend/src/staff/Staff_login.jsx
+++ No_due_frontend/src/staff/Staff_login.jsx
@@ -1,1 +1,11 @@
-// staff frondend login page
+import React from 'react'
+
+function Staff_login() {
+  return (
+    <div>
+         <h1>Staff login</h1>
+    </div>
+  )
+}
+
+export default Staff_login
