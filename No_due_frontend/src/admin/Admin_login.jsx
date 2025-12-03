907e11a0b23302049ddc94c335262f1fd694ce66
Arjun Biju <arjunbiju200611@gmail.com>
2025-12-03 15:13:09 +0530

    arjun's push

--- No_due_frontend/src/admin/Admin_login.jsx
+++ No_due_frontend/src/admin/Admin_login.jsx
@@ -4,6 +4,7 @@ function Admin_login() {
   return (
     <div>
         <h1>Admin login</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus dicta odio tempore atque perspiciatis asperiores ipsam impedit distinctio accusantium accusamus! Laborum harum doloribus asperiores similique animi? Minima facere earum perspiciatis?</p>
     </div>
   )
 }
