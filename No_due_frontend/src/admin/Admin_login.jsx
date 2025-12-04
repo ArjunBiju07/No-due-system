6b75a4dd81d6b8cc2d7e9d4a60dfa5052765bb39
Arjun Biju <arjunbiju200611@gmail.com>
2025-12-04 21:37:25 +0530

    admin login

--- No_due_frontend/src/admin/Admin_login.jsx
+++ No_due_frontend/src/admin/Admin_login.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Admin_login() {
   return (
     <div>
       <h1>hello worldddddddd</h1>
+      <Link to="/student_login">Go to </Link>
     </div>
   )
 }
