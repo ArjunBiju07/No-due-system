e6a941965f3edf914e319d73ac861b10a7f3c866
Arjun Biju <arjunbiju200611@gmail.com>
2025-12-04 23:23:33 +0530

    select role

--- No_due_frontend/src/Header.jsx
+++ No_due_frontend/src/Header.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import logo from './assets/logo.png';
+import React from 'react'
+import logo from "./assets/logo.png"
 
 function Header() {
   return (
     <div className="header shadow-sm py-2 px-3" style={{ backgroundColor: "#0d6efd" }}>
 
-      {/* Top Row */}
       <div className="d-flex justify-content-between align-items-center text-white">
 
-        {/* Logo + College Details */}
         <div className="d-flex align-items-center header-left">
           <img
             src={logo}
@@ -17,7 +15,7 @@ function Header() {
             style={{ width: '55px', background: "white", borderRadius: "5px" }}
           />
 
-          <div className="college-details" style={{ lineHeight: "1.1" }}>
+          <div className="college-details" style={{ lineHeight: "1.4" }}>
             <p className="mb-0 fw-bold" style={{ fontSize: "14px" }}>
               Govt. Polytechnic College Purappuzha
             </p>
@@ -29,30 +27,14 @@ function Header() {
             </p>
           </div>
         </div>
-
-        {/* Links */}
-        {/* <div className="link-bar d-flex align-items-center gap-2">
-          <span><ion-icon name="home" style={{ color: "white" }}></ion-icon></span>
-          <a href="/" className="text-decoration-none small text-white">Home</a>
-
-          <span><ion-icon name="accessibility" style={{ color: "white" }}></ion-icon></span>
-          <a href="/about" className="text-decoration-none small text-white">About</a>
-
-          <span><ion-icon name="call" style={{ color: "white" }}></ion-icon></span>
-          <a href="/contact" className="text-decoration-none small text-white">Contact</a>
-
-          <span><ion-icon name="call" style={{ color: "white" }}></ion-icon></span>
-          <a href="/contact" className="text-decoration-none small text-white">Profile</a>
-
-        </div> */}
       </div>
 
-      {/* Title */}
-      <h4 className="text-center mt-2 fw-bold" style={{ fontSize: "40px", color: "white" }}>
+      <h4 className="text-center  fw-bold" style={{ fontSize: "40px", color: "white" }}>
         No Due Clearence System
       </h4>
+      
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
